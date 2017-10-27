d5edf7927a39528082e5480ebe37ef2a57ec852f
William Rainaud <williamrainaud@Williams-Air.fios-router.home>
2017-10-27 14:37:34 -0400

    Added Friends Data

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+// Friends Data
+
+var friends = [
+        {
+        "name": "Bush",
+        "photo": "https://instagram.fewr1-4.fna.fbcdn.net/t51.2885-15/e35/11909337_1613042448957164_947845166_n.jpg",
+        "scores": [1,2,3,4,5,1,2,3,4,5]
+        },
+       {
+        "name": "Bert",
+        "photo": "https://instagram.fewr1-4.fna.fbcdn.net/t51.2885-15/e35/22344476_1328006327327176_6987101008140697600_n.jpg",
+        "scores": [1,2,3,4,5,1,2,3,4,5]
+       },
+       {
+        "name": "Mike P.",
+        "photo": "https://instagram.fewr1-4.fna.fbcdn.net/t51.2885-15/e35/18094976_272082903197417_9103100282413252608_n.jpg",
+        "scores": [1,2,3,4,5,1,2,3,4,5]
+       },
+       {
+        "name": "Eugene",
+        "photo": "https://instagram.fewr1-4.fna.fbcdn.net/t51.2885-15/e15/10784939_746422775445865_315574628_n.jpg",
+        "scores": [1,2,3,4,5,1,2,3,4,5]
+       },
+       {
+        "name": "Quray",
+        "photo": "https://instagram.fewr1-4.fna.fbcdn.net/t51.2885-15/e35/21690151_1956511507961965_7717149052214706176_n.jpg",
+        "scores": [1,2,3,4,5,1,2,3,4,5]
+       },
+       {
+        "name": "Kevin",
+        "photo": "https://instagram.fewr1-4.fna.fbcdn.net/t51.2885-15/e35/16229075_189869014827087_5473548094874320896_n.jpg",
+        "scores": [1,2,3,4,5,1,2,3,4,5]
+       },
+       {
+        "name": "Harry",
+        "photo": "https://instagram.fewr1-4.fna.fbcdn.net/t51.2885-15/e35/20225559_137701043483755_765487133493297152_n.jpg",
+        "scores": [1,2,3,4,5,1,2,3,4,5]
+       },
+       {
+        "name": "Shawn",
+        "photo": "https://instagram.fewr1-4.fna.fbcdn.net/t51.2885-15/e35/15876604_1212065262217753_2997038525725016064_n.jpg",
+        "scores": [1,2,3,4,5,1,2,3,4,5]
+       },
+       {
+        "name": "Kyle",
+        "photo": "https://instagram.fewr1-4.fna.fbcdn.net/t51.2885-15/e35/13098900_818907288214006_372723234_n.jpg",
+        "scores": [1,2,3,4,5,1,2,3,4,5]
+       },
+       {
+        "name": "Thom",
+        "photo": "https://instagram.fewr1-4.fna.fbcdn.net/t51.2885-15/e35/17934459_776050829228148_4210269570260271104_n.jpg",
+        "scores": [1,2,3,4,5,1,2,3,4,5]
+       }
+];
